e55d4c7d39b99e4c224d95ad2d629addc7f85f75
serkrivorotenko <serkrivorotenko@gmail.com>
2024-08-31 10:15:20 +0200

    60% of home page

---
+++ src/javascript/home.js
@@ -0,0 +1,59 @@
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+const swiper = new Swiper('.home__factory-slider', {
+    modules: [Navigation],
+    slidesPerView: 2,
+    spaceBetween: 20,
+    slidesOffsetBefore: 2,
+    navigation: {
+        nextEl: '.home__factory-slider__next',
+        prevEl: '.home__factory-slider__prev',
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        720: {
+            slidesPerView: 2,
+        },
+    },
+});
+
+const { innerWidth } = window;
+const isMobile = innerWidth < 1200;
+
+if (isMobile) {
+    const menuList = document.querySelector('.header__nav-list');
+    const submenuButtons = document.querySelectorAll('.header__nav-submenu__button');
+    submenuButtons.forEach((button) => {
+        button.addEventListener('click', (e) => {
+            e.preventDefault();
+            const currentSubmenu = button.querySelector('.header__nav-submenu');
+            const isOpened = !currentSubmenu.classList.contains('closed');
+            if (isOpened) {
+                currentSubmenu.classList.add('closed');
+                return;
+            }
+
+            const openedElement = menuList.querySelector('.header__nav-submenu:not(.closed)');
+            if (openedElement) {
+                openedElement.classList.add('closed');
+            }
+
+            currentSubmenu.classList.remove('closed');
+        });
+    });
+
+    const burgerButton = document.querySelector('.header__burger');
+    const nav = document.querySelector('.header__nav');
+    burgerButton.addEventListener('click', (e) => {
+        nav.classList.toggle('opened');
+        burgerButton.classList.toggle('opened');
+
+        const currentOverflow = document.body.style.getPropertyValue('overflow');
+        document.body.style.overflow = currentOverflow === 'hidden' ? 'initial' : 'hidden';
+    });
+}
